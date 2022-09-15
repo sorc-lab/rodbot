e9d029d5ca29299f246188d33114de4ef9a2d74e
macfisher <macfisher01@gmail.com>
2022-09-14 19:52:41 -0600

    RB-2: Crude fleeing logic when buffs expire. Requires manual restart.

--- Xp.lua
+++ Xp.lua
@@ -179,22 +179,21 @@ local function arraySlice (tbl, s, e)
 end
 
 function Xp.flee()
-    -- TODO: Try this for now. Triggers are not going off for some reason.
-    Xp.initFleeTriggers()
+    if Xp.CURRENT_MOVE > 1 then
+        Xp.stopPathing()
+        send("stop")
 
-    Xp.stopPathing()
-    send("stop")
+        -- Slices a new array starting with current position in current path.
+        local fleeMoves = arraySlice(Xp.CURRENT_PATH, Xp.CURRENT_MOVE, #Xp.CURRENT_PATH)
 
-    -- Slices a new array starting with current position in current path.
-    local fleeMoves = arraySlice(Xp.CURRENT_PATH, Xp.CURRENT_MOVE, #Xp.CURRENT_PATH)
-
-    -- TODO: Note that I have to make my own continuePath func here b/c continue path uese global state
-    --  Need to make it so all these functions take in their needed values, not globals.
-    fleeTimer = tempTimer(
-        .5,
-        function() Xp.continueFleeing("Sorcerer's Guild", fleeMoves) end,
-        true
-    )
+        -- TODO: Note that I have to make my own continuePath func here b/c continue path uese global state
+        --  Need to make it so all these functions take in their needed values, not globals.
+        fleeTimer = tempTimer(
+                .5,
+                function() Xp.continueFleeing("Sorcerer's Guild", fleeMoves) end,
+                true
+        )
+    end
 end
 
 -- TODO: *** THESE TRIGGERS ARE NOT TRIGGERING ***
@@ -212,13 +211,7 @@ function Xp.initFleeTriggers()
             function() Xp.fleeFromPesvintToGuild() end
     )
 
-  -- The hardened air around you is beginning to soften
-  -- Your Electric Field Spell Expires
-  -- Cannot find militia man,conscript,citizen
-  -- The hardened air around you softens
-  -- You are currently too mentally drained to cast Electric Field
-  -- You are currently too mentally drained to cast Air Steel
-  -- Your hands have lost their electrical energy.
+
 
   -- TODO: *** START MOVING THESE NOTES OFF TO A REGEN FUNCTION ***
     -- TODO: ADD A REGEN JUST TO SIT FOR 5 mins to let zone re-pop
@@ -252,6 +245,8 @@ function Xp.startPathing(path)
     reInitPathingTrigger = tempTrigger(
         "YOU HAVE ARRIVED AT YOUR DESTINATION: Pesvint Path",
         function()
+            echo("\nIs the reInit path working?\n")
+
             -- TODO: Not sure if these are needed here.
             killTrigger(continuePathTrigger)
             killTrigger(reInitPathingTrigger)
@@ -259,7 +254,28 @@ function Xp.startPathing(path)
         end
     )
 
-    --Xp.initFleeTriggers()
+    -- The hardened air around you is beginning to soften
+    -- Your Electric Field Spell Expires
+    -- Cannot find militia man,conscript,citizen
+    -- The hardened air around you softens
+    -- You are currently too mentally drained to cast Electric Field
+    -- You are currently too mentally drained to cast Air Steel
+    -- Your hands have lost their electrical energy.
+
+    airSteelDropTrigger = tempTrigger(
+            "The hardened air around you softens",
+            function() Xp.flee() end
+    )
+
+    electricFieldDropTrigger = tempTrigger(
+            "Your Electric Field Spell Expires",
+            function() Xp.flee() end
+    )
+
+    shockingGraspDropTrigger = tempTrigger(
+            "Your hands have lost their electrical energy",
+            function() Xp.flee() end
+    )
 end
 
 function Xp.stopPathing()
@@ -296,10 +312,12 @@ end
 -- TODO: This method needs be handed the next index. It cannot increment within, else use global.
 function Xp.continueFleeing(destination, moves)
     if Xp.FLEE_IDX > #moves then
+        send("stop")
         Xp.FLEE_IDX = 1
 
         cecho("\n<red:yellow>FLEE TO PESVINT!\n")
         disableTimer(fleeTimer)
+
     else
         --echo("\nFleeing: "..moves[Xp.FLEE_IDX].."\n")
         send(moves[Xp.FLEE_IDX])
